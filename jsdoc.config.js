1a19e1ddff31441b4102551ecd4fb1cf2c42a553
kappler <christoph.kappler@umweltbundesamt.at>
2018-12-13 13:57:23 +0100

    Added jsdoc and yarn script

---
+++ jsdoc.config.js
@@ -0,0 +1,21 @@
+/**
+ * Configuration for `jsdoc`
+ * @see http://usejsdoc.org/about-configuring-jsdoc.html
+ */
+module.exports = {
+  source: {
+    include: ['src'],
+    includePattern: '.+\\.js(doc|x)?$',
+  },
+  sourceType: 'module',
+  opts: {
+    // template: 'templates/default', // same as -t templates/default
+    encoding: 'utf8', // same as -e utf8
+    destination: './doc/', // same as -d ./out/
+    recurse: true, // same as -r
+  },
+  templates: {
+    cleverLinks: true,
+    monospaceLinks: false,
+  },
+};
