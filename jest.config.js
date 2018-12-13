8a75b217e32fea6d7578cc249f12fc880a1bc2ad
kappler <christoph.kappler@umweltbundesamt.at>
2018-12-13 10:26:25 +0100

    Added open layer as dependency

--- jest.config.js
+++ jest.config.js
@@ -171,9 +171,9 @@ module.exports = {
 
   // An array of regexp pattern strings that are matched against all source file paths,
   // matched files will skip transformation
-  // transformIgnorePatterns: [
-  //   "\\\\node_modules\\\\"
-  // ],
+  transformIgnorePatterns: [
+    '<rootDir>/node_modules/(?!ol/)',
+  ],
 
   // An array of regexp pattern strings that are matched against all modules before the
   // module loader will automatically return a mock for them
