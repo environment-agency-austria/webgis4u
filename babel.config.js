03d8392ccc30c88572769541eb0a7fd1c25d8414
kappler <christoph.kappler@umweltbundesamt.at>
2018-12-13 10:50:58 +0100

    Use babel.config.js instead of .babelrc

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+/**
+ * Configuration for `babel`
+ * @see https://babeljs.io/docs/en/configuration
+ */
+module.exports = {
+  presets: ['@babel/preset-env'],
+  plugins: [
+    '@babel/plugin-proposal-object-rest-spread',
+  ],
+};
