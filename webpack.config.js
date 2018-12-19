a0bb7f286ea09efd7972061f933e27972ff5ff9a
kappler <christoph.kappler@umweltbundesamt.at>
2018-12-19 14:29:58 +0100

    Improved configuration

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,17 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
+      {
+        test: /\.scss$/,
+        use: [
+          // creates style nodes from JS strings
+          { loader: 'style-loader' },
+          // translates CSS into CommonJS
+          { loader: 'css-loader' },
+          // compiles Sass to CSS
+          { loader: 'sass-loader' },
+        ],
+      },
     ],
   },
   resolve: {
