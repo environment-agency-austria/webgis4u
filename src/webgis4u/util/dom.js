4b8039df71160060596f605eaf1c41b413c28d50
kappler <christoph.kappler@umweltbundesamt.at>
2018-12-21 10:53:25 +0100

    feat: added utils for dom

---
+++ src/webgis4u/util/dom.js
@@ -0,0 +1,6 @@
+/**
+ * @fileoverview contains various DOM related util functions
+ * @module webgis4u/util/dom
+ */
+
+export * from './dom/createElement';
---
+++ src/webgis4u/util/dom/createElement.js
@@ -0,0 +1,38 @@
+/**
+ * @module webgis4u/util/dom/createElement
+ */
+
+import { hasValue } from '../string/hasValue';
+
+/**
+ * @typedef CreateHtmlElementOptions
+ * @property {string} tag - The tag
+ * @property {string} [cssClass] - The class of the element
+ */
+
+/**
+ * Util function to create a HTML element
+ *
+ * @param {CreateHtmlElementOptions} options
+ * The options
+ *
+ * @returns {HTMLElement} An html element
+ */
+export function createElement(options) {
+  const {
+    tag, cssClass,
+  } = options;
+
+  // Check if a valid tag was passed
+  if (!hasValue(tag)) {
+    throw new TypeError('tag must be a valid HTML tag');
+  }
+
+  // Create the element
+  const e = document.createElement(tag);
+
+  // Add css class
+  if (cssClass) { e.className = cssClass; }
+
+  return e;
+}
