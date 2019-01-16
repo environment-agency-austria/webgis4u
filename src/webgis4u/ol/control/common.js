94a85491229f15df95d7b1e1f8fbd19be63cecd3
kappler <christoph.kappler@umweltbundesamt.at>
2019-01-16 13:54:37 +0100

    feat: improved styling

--- src/webgis4u/ol/control/common.js
+++ src/webgis4u/ol/control/common.js
@@ -5,7 +5,7 @@
 /**
  * The default CSS class that OL uses for controls
  */
-export const CSS_CONTROL_DEFAULT = 'ol-control';
+export const OL_CSS_CONTROL_DEFAULT = 'ol-control';
 /**
  * The default CSS class that OL uses for contols
  * that can't be selected
@@ -16,3 +16,7 @@ export const CSS_CONTROL_UNSELECTABLE = 'ol-unselectable';
  * Prefix used for controls
  */
 export const CSS_CONTROL_PREFIX = 'webgis4u-control';
+
+export const OL_CONTROL_ZOOM_CSS_CLASS_MOUSE_POSITION = 'ol-mouse-position';
+export const OL_CONTROL_ZOOM_CSS_CLASS_ZOOM = 'ol-zoom';
+export const OL_CONTROL_ZOOM_CSS_CLASS_ZOOMSLIDER = 'ol-zoomslider';
