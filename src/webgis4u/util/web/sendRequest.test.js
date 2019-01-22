56e4f80f66ec6a673e4d24096f334219efe61a05
kappler <christoph.kappler@umweltbundesamt.at>
2019-01-22 16:17:00 +0100

    fix: removed unneeded comments

--- src/webgis4u/util/web/sendRequest.test.js
+++ src/webgis4u/util/web/sendRequest.test.js
@@ -84,18 +84,4 @@ describe('webgis4u/util/web/sendRequest', () => {
     xhrMock.onerror(xhrMock, { type: 'error' });
     expect(error).toBeCalled();
   });
-
-  // it ('should throw an error', () => {
-
-  // });
-
-  // it('with string', () => {
-  //   expect(encodeQueryData({ test: 'test' })).toBe('test=test');
-  //   expect(encodeQueryData({ test1: 'test1', test2: 'test2' })).toBe('test1=test1&test2=test2');
-  // });
-
-  // it('with number', () => {
-  //   expect(encodeQueryData({ test: 1 })).toBe('test=1');
-  //   expect(encodeQueryData({ test1: 1, test2: 2 })).toBe('test1=1&test2=2');
-  // });
 });
