7bec8386c5d75aa13b5d77c0c032a01b6f733510
kappler <christoph.kappler@umweltbundesamt.at>
2019-01-22 16:17:36 +0100

    feat: Added keycode enum

---
+++ src/webgis4u/util/dom/KeyCodeEnum.js
@@ -0,0 +1,16 @@
+/**
+ * @module webgis4u/util/dom/KeyCodeEnum
+ */
+
+/**
+ * @enum
+ */
+const KeyCodeEnum = {
+  ENTER: 13,
+  ARROW_UP: 38,
+  ARROW_DOWN: 40,
+};
+
+Object.freeze(KeyCodeEnum);
+
+export default KeyCodeEnum;
